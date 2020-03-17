9b26b179c8ecd3c9f117d08aa1eaa13434a50c23
mvongjesda <marsha.vongjesda@gmail.com>
2020-03-17 16:16:26 -0400

    Removed console.log and added d3.event.preventDefault

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,12 @@
 // from data.js
 let tableData = data;
 
-// Start of CODE HERE!
+// Reflect the data into the main table 
 let tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
     let row = tbody.append("tr");
     Object.entries(ufoSightings).forEach(function([key, value]) {
-        console.log(key, value);
         let cell = tbody.append("td");
         cell.text(value);
     });
@@ -19,18 +17,18 @@ let button = d3.select("#button");
 
 // Complete the click handler for selected input
 button.on("click", function() {
-
+    //Prevents page from reloading
+    d3.event.preventDefault();
+    
     d3.select(".overview").html("");
 
     let inputElement = d3.select("#datetime");
     let inputValue = inputElement.property("value");
-
     let selectedData = tableData.filter(tableData => tableData.datetime === inputValue);
 
     selectedData.forEach((searchDate) => {
         let row = tbody.append("tr");
         Object.entries(searchDate).forEach(([key, value]) => {
-            console.log(key, value);
             let cell = tbody.append("td");
             cell.text(value);
         });
