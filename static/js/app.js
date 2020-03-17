bab5339b2d5393b8f57ea94ec4a238254dee33c9
mvongjesda <marsha.vongjesda@gmail.com>
2020-03-17 15:27:35 -0400

    Updated date search

--- static/js/app.js
+++ static/js/app.js
@@ -4,14 +4,35 @@ let tableData = data;
 // Start of CODE HERE!
 let tbody = d3.select("tbody");
 
-console.log(data)
-
-data.forEach(function(ufoSightings) {
+tableData.forEach(function(ufoSightings) {
     console.log(ufoSightings);
     let row = tbody.append("tr");
     Object.entries(ufoSightings).forEach(function([key, value]) {
         console.log(key, value);
         let cell = tbody.append("td");
         cell.text(value);
+    });
+});
+
+// Select the button
+let button = d3.select("#button");
+
+// Complete the click handler for selected input
+button.on("click", function() {
+
+    d3.select(".overview").html("");
+
+    let inputElement = d3.select("#datetime");
+    let inputValue = inputElement.property("value");
+
+    let selectedData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    selectedData.forEach((searchDate) => {
+        let row = tbody.append("tr");
+        Object.entries(searchDate).forEach(([key, value]) => {
+            console.log(key, value);
+            let cell = tbody.append("td");
+            cell.text(value);
+        });
     });
 });
